8c69504e4889e4ac88d254dbf1b222f8eb71d71c
Dhanajit kapali <dhanajitkapali30@gmail.com>
2021-08-10 15:07:31 +0530

    Initial Commit

---
+++ PreventScreenCapture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PreventScreenCapture
+//
+//  Created by unthinkable-mac-0025 on 10/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
