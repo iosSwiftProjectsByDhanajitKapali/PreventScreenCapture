5d9175e5f519fe26be5bf49dce434cbb5fcff93d
Dhanajit kapali <dhanajitkapali30@gmail.com>
2021-08-10 16:59:39 +0530

    added code to present a warning screen if user takes screenshot

--- PreventScreenCapture/AppDelegate.swift
+++ PreventScreenCapture/AppDelegate.swift
@@ -11,9 +11,16 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
+    private let screenProtecter = ScreenCaptureProtector()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        //Code to Prevent Screen Recording
+        screenProtecter.startPreventingRecording()
+        screenProtecter.startPreventingScreenshot()
+        
         return true
     }
 
--- PreventScreenCapture/ViewController.swift
+++ PreventScreenCapture/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //ScreenCaptureProtector(withObserver: self).startPreventingScreenshot()
+        
+        //NotificationCenter.default.addObserver(self, selector: #selector(didTakeScreenshot), name: UIApplication.userDidTakeScreenshotNotification, object: nil)
+        
+        //NotificationCenter.default.addObserver(self, selector: #selector(didDetectRecording), name: UIScreen.capturedDidChangeNotification, object: nil)
+        
+        
     }
+    
+   
+}
 
-
+extension ViewController{
+    @objc func didTakeScreenshot() -> Void {
+        print("Screen Shot Taken")
+    }
+    
+    @objc func didDetectRecording() -> Void{
+        print("Screen Recording Started")
+    }
 }
 
